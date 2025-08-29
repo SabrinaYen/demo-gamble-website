a59eb4bd5725cf05bde633ea08211a46fbb12b6b
sabrina <sabrinayen1301@gmail.com>
2025-08-30 00:19:14 +0800

    udpate

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -92,22 +92,22 @@ function showPopup(gameSeq) {
   const message = [
     {
       title: "Chasing Fortune",
-      pic: "./assets/images/station1.jpeg",
+      pic: "./assets/images/step1-instruct.jpeg",
       desc: "You will play 5 matches for this game. Each group begins with 5 tokens, and each draw costs 1 token. If you draw a WIN card, you get back your original token plus one extra. If you draw a LOSE card, you lose 1 token. If you draw a NEARLY WIN card, you only get back your original token. If you draw a BIG WIN card, you get back your original token plus two extra.",
     },
     {
       title: "Bet your way out",
-      pic: "./assets/images/station2.jpeg",
+      pic: "./assets/images/step2-instruct.jpeg",
       desc: "Each team draws five cards, and the team with the higher total wins the round. The game will be played in three rounds, and the team that wins two rounds first will be the winner. The losing team MUST give the winning team 8 tokens. Then, the winning team will move on to Activity 3, while the losing team will move directly to Activity 4.",
     },
     {
       title: "Double it or Lose it All",
-      pic: "./assets/images/station3.jpeg",
+      pic: "./assets/images/step3-instruct.jpeg",
       desc: "Congratulations you have made it this far!\nNow‘s your chance to double however much token you have in hand.\n\nYou have 1 chance to press the buzzers.\n\n2 of 3 of the buzzers are “DOUBLE IT ALL” and one is “ LOSE IT ALL “\n\nYou win when you hear “ DOUBLE IT ALL” and lose all your money if you hear “ LOSE IT ALL. ",
     },
     {
       title: "From Curiosity To Collapse",
-      pic: "./assets/images/station4.jpeg",
+      pic: "./assets/images/step4-instruct.jpeg",
       desc: "Rearrange the events in the right order using the clues you have. After that, show it to the PIC.",
     },
   ];
